7e425f23e49c595644f67d32a8c5a570562dea38
Debraj Mukhopadhyay <quic_dmukhopa@quicinc.com>
2022-06-27 12:24:25 -0700

    Prevent null lockscreen credential
    The getPasswordHistoryHashFactor API expects a non-null credential
    parameter. If the existing credential was previously set to none, the
    credential will be null. Therefore, it is necessary to check for this
    null state and replace it with a "none" lockscreen credential.
    
    Bug: 230576954
    Test: manual
    Change-Id: Ia99547c1c93aed41471be1c0f074458e8d3de9ae
    (cherry picked from commit 1560c4dc7ee083a1d50953b46bc9662ea759d517)
    (cherry picked from commit 438d865452b5109e058a593944bfef6c9c4522e1)

--- src/com/android/car/settings/security/PasswordHelper.java
+++ src/com/android/car/settings/security/PasswordHelper.java
@@ -136,7 +136,7 @@ public class PasswordHelper {
     private byte[] getPasswordHistoryHashFactor(LockscreenCredential credential) {
         if (mPasswordHistoryHashFactor == null) {
             mPasswordHistoryHashFactor = mLockPatternUtils.getPasswordHistoryHashFactor(
-                    credential, mUserId);
+                    credential != null ? credential : LockscreenCredential.createNone(), mUserId);
         }
         return mPasswordHistoryHashFactor;
     }
