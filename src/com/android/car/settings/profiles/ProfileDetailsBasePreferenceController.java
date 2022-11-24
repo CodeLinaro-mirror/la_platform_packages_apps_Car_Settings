2c44fb014ef2124ee0ef3862a44dfbeba7a61f1a
Kihyung Lee <kihyunglee@google.com>
2022-11-24 11:33:39 +0900

    Move some initialization/cleanup code to safer place
    onDestroyInternal() is not a safe place to cleanup lifecycle-related code.
    Let's move some cleanup code to safer place, onStopInternal()
    Some initializatin code was to be moved to onStartInternal() respectively.
    
    UserInfo and UI need to be updated and refreshed when restarting not to
    miss any profile changes while being stopped.
    
    Test: Run Settings, open "Profiles & accounts" fragment.
          "adb shell input keyevent KEYCODE_BACK" to close Settings.
          Previously, Settings got crashed while onDestroyInternal().
          After this fix, Settings will close without error.
          Check if changes in profile are correctly reflected in UI.
          (e.g. rename the current account)
    
    Bug: 254805074
    Bug: 260952981
    
    Change-Id: Ib523bf0aed1086006b26c63aae4e90e6c0333bd3
    (cherry picked from commit bb28cdf63d7683d42f5d926a9c37c0c343765922)
    Signed-off-by: Arun Raj Shanmugam <quic_arunshan@quicinc.com>

--- src/com/android/car/settings/profiles/ProfileDetailsBasePreferenceController.java
+++ src/com/android/car/settings/profiles/ProfileDetailsBasePreferenceController.java
@@ -38,6 +38,7 @@ public abstract class ProfileDetailsBasePreferenceController<V extends Preferenc
         PreferenceController<V> {
 
     private UserInfo mUserInfo;
+    private boolean mNeedToRefreshUserInfo = false;
 
     private final BroadcastReceiver mProfileUpdateReceiver = new BroadcastReceiver() {
         @Override
@@ -76,16 +77,23 @@ public abstract class ProfileDetailsBasePreferenceController<V extends Preferenc
 
     /** Registers a listener which updates the displayed profile name when a profile is modified. */
     @Override
-    protected void onCreateInternal() {
+    protected void onStartInternal() {
         registerForProfileEvents();
+
+        /* refresh UserInfo and UI only when restarting */
+        if (mNeedToRefreshUserInfo) {
+            refreshUserInfo();
+            refreshUi();
+        }
     }
 
     /**
      * Unregisters a listener which updates the displayed profile name when a profile is modified.
      */
     @Override
-    protected void onDestroyInternal() {
+    protected void onStopInternal() {
         unregisterForProfileEvents();
+        mNeedToRefreshUserInfo = true;
     }
 
     private void registerForProfileEvents() {
