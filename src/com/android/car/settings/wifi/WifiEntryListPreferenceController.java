a151ddfbd7ce8ae1f857c7e23617e18dd457880b
Zhongyue Gao <zhongao@qti.qualcomm.com>
2026-02-26 00:23:28 +0800

    Wifi(CarSettings): Support WPA2/3 Enterprise & 192-bit connection
    Add support for WPA2/3 Enterprise & 192-bit connection.
    This commit combines 7 previous commits into one.
    * a7b187ad Wifi: Add AndroidKeystore for enterprise connect support
    * 470e6d77 wifi(CarSettings): Support EAP connection from UI
    * e7970dcf Wifi(CarSettings): Add hidden enterprise AP connection support
    * fa32abf2 Wifi(CarSettings): Support WPA3 Enterprise & 192-bit connection
    * bde9b16b CarSettings: Add Wi-Fi certificate pre-install mechanism on UI
    * f52591ad Wifi(CarSettings): Add WPA3_ENTERPRISE & SuiteB support for h..
    * 62d131ad Wifi: Do eap method selects restrict in hidden SUITE_B
    
    CRs-Fixed: 3303256
    Change-Id: I5848327e1a506fe94692b2c99aa10c447df4043b

--- src/com/android/car/settings/wifi/WifiEntryListPreferenceController.java
+++ src/com/android/car/settings/wifi/WifiEntryListPreferenceController.java
@@ -12,6 +12,10 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ *
+ * Changes from Qualcomm Technologies, Inc. are provided under the following license:
+ * Copyright (c) Qualcomm Technologies, Inc. and/or its subsidiaries.
+ * SPDX-License-Identifier: BSD-3-Clause-Clear
  */
 
 package com.android.car.settings.wifi;
@@ -74,6 +78,24 @@ public class WifiEntryListPreferenceController extends
                     }
                 }
             };
+
+    @VisibleForTesting
+    final WifiEnterpriseDialog.WifiDialogListener mEapDialogListener =
+            new WifiEnterpriseDialog.WifiDialogListener() {
+                @Override
+                public void onSubmit(WifiEnterpriseDialog dialog) {
+                    WifiConfiguration config = dialog.getWifiConfiguration();
+                    WifiEntry wifiEntry = dialog.getWifiEntry();
+                    if (config == null) {
+                        wifiEntry.connect(
+                                new WifiEntryConnectCallback(wifiEntry,
+                                        /* editIfNoConfig= */ false));
+                    } else {
+                        getWifiManager().connect(config, mConnectionListener);
+                    }
+                }
+            };
+
 
     private List<WifiEntry> mWifiEntries = new ArrayList<>();
 
@@ -175,10 +197,18 @@ public class WifiEntryListPreferenceController extends
                         getFragmentController().launchFragment(
                                 WifiDetailsFragment.getInstance(wifiEntry));
                     }
-                } else if (wifiEntry.shouldEditBeforeConnect()) {
-                    getFragmentController().showDialog(
-                            new WifiPasswordDialog(wifiEntry, mDialogListener),
-                            WifiPasswordDialog.TAG);
+                 } else if (wifiEntry.shouldEditBeforeConnect()) {
+                    if (wifiEntry.getSecurity() == WifiEntry.SECURITY_EAP ||
+                        wifiEntry.getSecurity() == WifiEntry.SECURITY_EAP_WPA3_ENTERPRISE ||
+                        wifiEntry.getSecurity() == WifiEntry.SECURITY_EAP_SUITE_B) {
+                        getFragmentController().showDialog(
+                                new WifiEnterpriseDialog(wifiEntry, mEapDialogListener),
+                                WifiEnterpriseDialog.TAG);
+                    } else {
+                        getFragmentController().showDialog(
+                                new WifiPasswordDialog(wifiEntry, mDialogListener),
+                                WifiPasswordDialog.TAG);
+                    }
                 } else {
                     wifiEntry.connect(
                             new WifiEntryConnectCallback(wifiEntry, /* editIfNoConfig= */ true));
@@ -220,9 +250,17 @@ public class WifiEntryListPreferenceController extends
 
             if (status == WifiEntry.ConnectCallback.CONNECT_STATUS_FAILURE_NO_CONFIG) {
                 if (mEditIfNoConfig) {
-                    getFragmentController().showDialog(
-                            new WifiPasswordDialog(mConnectWifiEntry, mDialogListener),
-                            WifiPasswordDialog.TAG);
+                    if (mConnectWifiEntry.getSecurity() == WifiEntry.SECURITY_EAP ||
+                        mConnectWifiEntry.getSecurity() == WifiEntry.SECURITY_EAP_WPA3_ENTERPRISE ||
+                        mConnectWifiEntry.getSecurity() == WifiEntry.SECURITY_EAP_SUITE_B) {
+                        getFragmentController().showDialog(
+                                new WifiEnterpriseDialog(mConnectWifiEntry, mEapDialogListener),
+                                WifiEnterpriseDialog.TAG);
+                    } else {
+                        getFragmentController().showDialog(
+                                new WifiPasswordDialog(mConnectWifiEntry, mDialogListener),
+                                WifiPasswordDialog.TAG);
+                    }
                 }
             } else if (status == CONNECT_STATUS_FAILURE_UNKNOWN) {
                 Toast.makeText(getContext(), R.string.wifi_failed_connect_message,
